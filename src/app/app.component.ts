b077501c33ba56262b04dd9bf545727bd8c53020
tishko <ttonev@infragistics.com>
2024-10-15 15:20:59 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IgxButtonGroupComponent } from 'igniteui-angular';
+import { Card } from './card.blueprint';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AIBEST';
+  title = 'your-app-name';
+  selectedButtonLabel: string = 'Student';
+  showRegisterForm: boolean = false;
+  public card = new Card({
+    content: `Hi! I'm Jane, photographer and filmmaker.
+    Photography is a way of feeling, of touching,
+    of loving. What you have caught on film is captured forever...
+    it remembers little things, long after you have
+    forgotten everything.`,
+    icons: ['person', 'favorite', 'share'],
+    imageUrl: 'https://images.unsplash.com/'
+    + 'photo-1541516160071-4bb0c5af65ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    subtitle: 'Professional Photographer',
+    title: 'Jane Doe'
+});
+
+  onSelectionChange(event:any) {
+    if(event.index == 0){
+      this.selectedButtonLabel = 'Student';
+    }
+    else if(event.index == 1){
+      this.selectedButtonLabel = 'Teacher';
+    }
+    else if(event.index == 2){
+      this.selectedButtonLabel = 'Admin';
+    }
+    if(this.showRegisterForm){
+      this.showRegisterForm = false
+    }
+  }
+
+  toggleRegisterForm() {
+    this.showRegisterForm = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxInputGroupModule, IgxButtonModule, IgxButtonGroupModule, IgxButtonGroupComponent, IgxCardModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxInputGroupModule,
+    IgxButtonModule,
+    IgxButtonGroupModule,
+    IgxCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
